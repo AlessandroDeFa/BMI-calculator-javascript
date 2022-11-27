6ae3dde730c483b6e20d305854356f6f29678d84
AlessandroDeFa <aledefaale@gmail.com>
2022-11-27 16:37:00 +0100

    complete the script
    Added a validation for the empty values with a error message showing up.

--- main.js
+++ main.js
@@ -3,10 +3,7 @@ const height = document.querySelector('#height');
 const button = document.querySelector('.button');
 const result = document.querySelector('.result');
 const comment = document.querySelector('.comment-span');
-
-// dafault variables
-let total = '';
-
+const error = document.querySelector('#error-message');
 
 // adding click listeners
 button.addEventListener('click', ()=>{
@@ -18,13 +15,27 @@ button.addEventListener('click', ()=>{
 const calculateBmi = ()=>{
   let heightValue = height.value;
   let weightValue = weight.value;
+  let total = '';
+  // condition if empty 
+  if(heightValue == '' || weightValue == ''){
+    error.style.display = 'block';
+    comment.innerText = '';
+    result.innerText = total;
+    return; 
+  }else {
+    error.style.display = 'none';
+  }
+
+  // calculation
   heightValue = heightValue / 100;
   total = weightValue / (heightValue * heightValue);
   total = total.toFixed(2);
   result.innerText = total;
-  // condition 
+
+  // condition result
+
   if(total < 18.5){
-    comment.innerText = "Underweight"
+    comment.innerText = "Underweight";
   }else if(total >= 18.5 && total <= 25){
     comment.innerText = "Normal weight"
   }else if(total > 25 && total <= 30){
@@ -32,6 +43,6 @@ const calculateBmi = ()=>{
   }else if(total > 30){
     comment.innerText = "Obese"
   };
-   
+  
 }
 
